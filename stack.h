75acd52a81844a1a94c2752ea2a607a8a2fd8341
haozhi <wisdomh226@163.com>
2019-01-22 21:52:37 +0800

    achieve the stack

--- stack.h
+++ stack.h
@@ -1,4 +1,22 @@
 #ifndef STACK_H_
 #define STACK_H_
 
-typedef unsigned long Item
+typedef unsigned long Item;
+
+class Stack
+{
+    private:
+        enum {MAX = 10}; //constant specific to class
+        Item items[MAX]; //holds stack items
+        int top; //indexx for top stack item
+    public:
+        Stack();
+        bool isempty() const;
+        bool isfull() const;
+        // push() returns false if stack already is full, true otherwise
+        bool push(const Item & item); // add item to stack
+        // pop() reuturns false if stack already is empty, ture otherwise
+        bool pop(Item & item); //pop top into item
+};
+
+#endif
